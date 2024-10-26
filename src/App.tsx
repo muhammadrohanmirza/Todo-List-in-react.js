ef3d2862c7e5bd32f92b39acb5331e1d80c3a918
Muhammad Rohan Mirza <m.rohanmirza2007@gmail.com>
2024-10-27 00:13:54 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TodoApp from "./component/Todo"
+import "./App.css"
+
+
+export default function App (){
+    return(
+        <div className="app">
+            <h1>My Todo List</h1>
+            <TodoApp />
+        </div>
+    )
+}
